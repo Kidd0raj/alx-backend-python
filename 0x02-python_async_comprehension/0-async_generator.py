e8284129c4205c1e7fd31ae011145c43354bbdf7
rajy <kiddo.rajy@gmail.com>
2024-01-17 13:36:55 +0000

    Added comments

--- 0x02-python_async_comprehension/0-async_generator.py
+++ 0x02-python_async_comprehension/0-async_generator.py
@@ -1,14 +1,20 @@
-#!/usr/bin/env python3
-'''Task 0's module.
+sk 0 module.
+
+This module defines an asynchronous generator function that yields a sequence
+of 10 random floating-point numbers between 0 and 10.
 '''
+
 import asyncio
 import random
 from typing import Generator
 
-
 async def async_generator() -> Generator[float, None, None]:
-        '''Generates a sequence of 10 numbers.
-            '''
-                for _ in range(10):
-                            await asyncio.sleep(1)
-                                    yield random.random() * 10
+        '''Generate a sequence of 10 random numbers.
+
+            Yields:
+                        float: A random floating-point number between 0 and 10.
+                            '''
+                                for _ in range(10):
+                                        await asyncio.sleep(1)
+                                                yield random.uniform(0, 10)
+
